79b91ffee19c4458650951e56e967ffb64490342
dhimanray <dray1@uci.edu>
2023-01-27 12:11:01 +0100

    Notebook update and remove wrong 13 descriptor models

--- OAMe_G2/13descriptor_together/source_libtorch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-LIBTORCH=/home/dray@iit.local/plumed-2.9-libtorch/plumed2/libtorch
-source ${LIBTORCH}/sourceme.sh
-source /home/dray@iit.local/plumed-2.9-libtorch/plumed2/sourceme.sh
