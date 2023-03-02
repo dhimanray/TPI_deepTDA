6cfa710402db5b720ed034502ac8f1807bdd5585
dhimanray <dray1@uci.edu>
2023-03-02 16:02:20 +0100

    update plumed input files

--- chignolin/training/flooding/Contact_CV/template/run_gromacs.sh
+++ chignolin/training/flooding/Contact_CV/template/run_gromacs.sh
@@ -7,4 +7,4 @@ gmx_mpi grompp -f md.mdp -c prd.gro -p topol_01.top -o prd.tpr
 
 export OMP_NUM_THREADS=1
 
-mpiexec -n 1 gmx_mpi mdrun -deffnm prd -plumed plumed_rate.dat -pin on -pinoffset CORE
+mpiexec -n 1 gmx_mpi mdrun -deffnm prd -plumed plumed.dat -pin on -pinoffset CORE
